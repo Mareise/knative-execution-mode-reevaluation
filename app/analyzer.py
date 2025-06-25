f545b496bafd47764058d09a53f549d633046999
maxi <maxi.reisecker@gmail.com>
2025-06-25 16:47:50 +0200

    Use kubernetes plugin to access services

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,28 +1,52 @@
 import requests
 import os
+from kubernetes import client, config
 
 PROMETHEUS_URL = os.environ.get("PROMETHEUS_URL", "http://localhost:9090")
-FUNCTION_NAME = "myfunc"
 
-def query_prometheus():
-    print("Haaallloooo")
-    url = f"{PROMETHEUS_URL}/api/v1/query"
-    print(url)
-    query = f'avg_over_time(function_execution_duration_seconds{{function_name="{FUNCTION_NAME}"}}[10m])'
+
+def get_knative_services():
+    config.load_incluster_config()
+
+    api = client.CustomObjectsApi()
+    kn_objects = api.list_cluster_custom_object(
+        group="serving.knative.dev",
+        version="v1",
+        plural="services"
+    )
+
+    names = [item["metadata"]["name"] for item in kn_objects["items"]]
+    print(names)
+
+    return names
+
+
+def query_service_metrics(service_name):
+    query = f'avg_over_time(function_execution_duration_seconds{{function_name="{service_name}"}}[10m])'
+
+
     response = requests.get(
         f"{PROMETHEUS_URL}/api/v1/query",
         params={"query": query}
     )
     response.raise_for_status()
-    result = response.json()
-    print("Raw Prometheus response:")
-    print(result)
+    return response.json()
 
-    if result["data"]["result"]:
-        value = float(result["data"]["result"][0]["value"][1])
-        print(f"\nAverage execution time for {FUNCTION_NAME} over last 10m: {value:.3f} seconds")
-    else:
-        print(f"\nNo data found for {FUNCTION_NAME}")
+
+def query_prometheus(service_names):
+    for service in service_names:
+        print(f"\nQuerying metrics for service: {service}")
+        result = query_service_metrics(service)
+        print("Raw Prometheus response:")
+        print(result)
+
+        if result["data"]["result"]:
+            value = float(result["data"]["result"][0]["value"][1])
+            print(f"Average execution time for {service} over last 10m: {value:.3f} seconds")
+        else:
+            print(f"No data found for {service}")
+
 
 if __name__ == "__main__":
-    query_prometheus()
+    services = get_knative_services()
+    query_prometheus(services)
