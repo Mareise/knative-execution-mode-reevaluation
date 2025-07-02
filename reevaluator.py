ef2b937ff23b25dd0aca3dc21e6636364530d181
maxi <maxi.reisecker@gmail.com>
2025-07-02 16:17:09 +0200

    Handle none case

--- reevaluator.py
+++ reevaluator.py
@@ -10,12 +10,14 @@ INTERVAL_SECONDS = int(os.environ.get("INTERVAL_SECONDS", "60"))
 def reevaluate(services):
     for service in services:
         print(f"\nQuerying metrics for service: {service.name}")
-        value = query_service_metrics(service, QUERIES["latency_avg"](service.name, window="5m"))
+        window = "5m"
+        value = query_service_metrics(service, QUERIES["latency_avg"](service.name, window))
 
-        print(f"Average execution time for {service.name} over last 5m: {value:.3f} ms")
-        if value > 100:
-            print("WARNING: Execution time is above 100ms")
-            patch_knative_service(service.name, 1, "gpu_preferred", service.namespace)
+        if value is not None:
+            print(f"Average execution time for {service.name} over last {window}: {value:.3f} ms")
+            if value > 100:
+                print("WARNING: Execution time is above 100ms")
+                patch_knative_service(service.name, 1, "gpu_preferred", service.namespace)
 
 
 if __name__ == "__main__":
