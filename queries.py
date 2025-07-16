b9a845fb28e25a666276c92aa1da642aad0e6b8b
maxi <maxi.reisecker@gmail.com>
2025-07-16 16:02:23 +0200

    Use env-vars as thresholds

--- queries.py
+++ queries.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 from enum import Enum
 
@@ -18,19 +19,32 @@ QUERIES = {
     ),
 }
 
-QUERY_RESULT = namedtuple("QueryResult",
-                          ["query_result_short_interval", "query_result_long_interval", "new_mode_query_result"])
+QUERY_RESULT = namedtuple(
+    "QueryResult", ["query_result_short_interval", "query_result_long_interval", "new_mode_query_result"]
+)
 
 # TODO different performance_change_gap dependent on current execution mode
-QUERY_THRESHOLD = namedtuple("QueryThreshold", ["upper_bound", "upper_bound_when_low_request_rate", "lower_bound",
-                                                "performance_change_gap"])
+QUERY_THRESHOLD = namedtuple(
+    "QueryThreshold", ["upper_bound", "upper_bound_when_low_request_rate", "lower_bound", "performance_change_gap"]
+)
 
 # TODO Upper bound sollte glaub ich höher sein (cold start ziemlich lange (model fetching))
 QUERY_THRESHOLDS = {
-    QueryNames.LATENCY_AVG: QUERY_THRESHOLD(upper_bound=30, upper_bound_when_low_request_rate=100, lower_bound=None,
-                                            performance_change_gap=10),
-    QueryNames.REQUEST_RATE: QUERY_THRESHOLD(upper_bound=None, upper_bound_when_low_request_rate=None, lower_bound=0.01,
-                                             performance_change_gap=None),
+    QueryNames.LATENCY_AVG:
+        QUERY_THRESHOLD(
+            upper_bound=int(os.environ.get("THRESHOLD_LATENCY_UPPER", "30")),
+            upper_bound_when_low_request_rate=int(
+                os.environ.get("THRESHOLD_LATENCY_UPPER_WHEN_LOW_REQUEST_RATE", "100")),
+            lower_bound=None,
+            performance_change_gap=int(os.environ.get("THRESHOLD_LATENCY_PERFORMANCE_CHANGE_GAP", "10"))
+        ),
+    QueryNames.REQUEST_RATE:
+        QUERY_THRESHOLD(
+            upper_bound=None,
+            upper_bound_when_low_request_rate=None,
+            lower_bound=float(os.environ.get("THRESHOLD_REQUEST_RATE_LOWER_BOUND", "0.01")),
+            performance_change_gap=None
+        ),
 }
 
-LONG_INTERVAL_MULTIPLIER = 50
+LONG_INTERVAL_MULTIPLIER = int(os.environ.get("LONG_INTERVAL_MULTIPLIER", "50"))
